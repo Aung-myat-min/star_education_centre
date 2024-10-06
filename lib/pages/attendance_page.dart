a4873259a7962e9110c97a9aeb1ac71f9016026b
Aung-myat-min <koaungmyatmin0@gmail.com>
2024-10-06 15:03:26 +0630

    feat: page skeletons

---
+++ lib/pages/attendance_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AttendancePage extends StatefulWidget {
+  const AttendancePage({super.key});
+
+  @override
+  State<AttendancePage> createState() => _AttendancePageState();
+}
+
+class _AttendancePageState extends State<AttendancePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("Attendance Page"),
+    );
+  }
+}
---
+++ lib/pages/courses_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CoursesPage extends StatefulWidget {
+  const CoursesPage({super.key});
+
+  @override
+  State<CoursesPage> createState() => _CoursesPageState();
+}
+
+class _CoursesPageState extends State<CoursesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("Courses Page"),
+    );
+  }
+}
